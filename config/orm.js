b8d80500983d92bb3f393347e4c7c6bf3d3c9448
JEverett <jonathan@jonathaneverettcreative.com>
2019-04-24 16:24:04 -0500

    Set up folder and file structure, created initial orms (will need tweaking) and a function in orm.js for generating object structure on value input when updating a SQL column. Database populated with initial 3 data.

---
+++ config/orm.js
@@ -0,0 +1,56 @@
+var connection = require("./connection");
+
+function objToSql(obj) {
+    var arr = [];
+
+    for (var key in obj) {
+        var value = obj[key];
+
+        if (Object.hasOwnProperty.call(obj, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + value);
+        }
+    }
+}
+
+var orm = {
+    selectAll: function(tableInput) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function(table, cols, val, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (" + val + ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, val, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function(table, objColVal, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVal);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+}
